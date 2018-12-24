e278c25e2402716d683fde0529960be56ba40e97
Juan Ramon <juanras89s@gmail.com>
2018-12-24 14:10:51 +0000

    Added a view for listing all users and modified show of articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
 
-    def gravatar_for(user, options = { size: 80 })
+  def gravatar_for(user, options = { size: 80, styles: "" })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
+    additional_styles = options[:styles]
+
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.username, class: "rounded-circle")
+    image_tag(gravatar_url, alt: user.username, class: "rounded-circle #{additional_styles}")
   end
 end
