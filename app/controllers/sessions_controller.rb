9be38383f2266a1af6889b9b5ef843225a069b14
Juan Ramon <juanras89s@gmail.com>
2018-12-25 22:39:35 +0000

    Added login and logout functions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have sucessfully logged in"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "The credentials you have entered are wrong"
+      render 'new'
+    end
   end
 
   def destroy
-    
+    debugger
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
   end
 end
