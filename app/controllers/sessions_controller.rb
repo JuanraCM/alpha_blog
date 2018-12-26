ff1cd0e3550ac452f28c4b255b513033a26b813d
Juan Ramon <juanras89s@gmail.com>
2018-12-26 11:35:40 +0000

    Added restrictions in the UI for non-logged users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,6 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    debugger
     session[:user_id] = nil
     flash[:success] = "You have logged out"
     redirect_to root_path
