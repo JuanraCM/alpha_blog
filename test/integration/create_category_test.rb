725979a648cb55c2ef74363f9cf32dcfa2ac8eab
JuanraCM <juanras89s@gmail.com>
2019-01-02 13:35:27 +0000

    Added a new test for invalid categories

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -15,4 +15,17 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_match "sports", response.body
   end
 
+  test "invalid category submission results in failure" do
+    get new_category_path
+    assert_template 'categories/new'
+
+    assert_no_difference 'Category.count' do
+        post categories_path, params: { category: { name: "" } }
+    end
+
+    assert_template 'categories/new'
+    assert_select 'div#errors-panel'
+    assert_select 'div.panel-body'
+  end
+
 end
